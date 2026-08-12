49604052cfd526bde5718ff1d40101f01f27afbc
Chad Wagoner <chawagon03@gmail.com>
2026-08-11 21:26:42 -0400

    chore(nix): enable gc and store optimisation

--- modules/common.nix
+++ modules/common.nix
@@ -1,10 +1,18 @@
-{ ... }:
+{ config, pkgs, ... }:
 
 {
   nix.settings.experimental-features = [
     "nix-command"
     "flakes"
   ];
+
+  nix.gc = {
+    automatic = true;
+    dates = "weekly";
+    options = "--delete-older-than 30d";
+  };
+
+  nix.optimise.automatic = true;
 
   environment.systemPackages = with pkgs; [
     curl
