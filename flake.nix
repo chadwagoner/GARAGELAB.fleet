2b70fbd50fdb81e2a7c8d01474b4425f41a80bc1
Chad Wagoner <chawagon03@gmail.com>
2026-08-11 21:48:28 -0400

    feat(nixos): enable auto-upgrade module

--- flake.nix
+++ flake.nix
@@ -14,6 +14,7 @@
           ./modules/common.nix
           ./modules/ssh.nix
           ./modules/users.nix
+          ./modules/auto-upgrade.nix
           ./hosts/cobra/configuration.nix
         ];
       };
