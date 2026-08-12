594faf81c99f81a123880c5103ee108029fd206a
Chad Wagoner <chawagon03@gmail.com>
2026-08-12 00:57:26 -0400

    feat(cobra): add Plex service

---
+++ services/service_plex.nix
@@ -0,0 +1,40 @@
+{ lib, ... }:
+
+{
+  virtualisation.oci-containers.containers.plex = {
+    image = "ghcr.io/linuxserver/plex:1.43.3";
+    networks = [ "host" ];
+    devices = [ "/dev/dri:/dev/dri" ];
+    environment = {
+      PGID = "100";
+      PUID = "1000";
+      VERSION = "docker";
+    };
+    volumes = [
+      "/etc/localtime:/etc/localtime:ro"
+      "plex-config:/config"
+      "/srv/media:/media"
+    ];
+  };
+
+  systemd.services.podman-plex = {
+    unitConfig.RequiresMountsFor = [ "/srv/media" ];
+    serviceConfig.Restart = lib.mkForce "always";
+  };
+
+  networking.firewall = {
+    allowedTCPPorts = [
+      8324
+      32400
+      32469
+    ];
+    allowedUDPPorts = [
+      1900
+      5353
+      32410
+      32412
+      32413
+      32414
+    ];
+  };
+}
