2f063347abb199fd31e7c10bc48a588a160fdc56
Chad Wagoner <chawagon03@gmail.com>
2026-08-13 01:07:34 -0400

    feat(nixos): add agenix and tailscale

--- flake.nix
+++ flake.nix
@@ -3,18 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-26.05";
+
+    agenix = {
+      url = "github:ryantm/agenix";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
-  outputs = { self, nixpkgs, ... }:
+  outputs = inputs@{ self, nixpkgs, agenix, ... }:
     {
       nixosConfigurations.cobra = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
 
+        specialArgs = {
+          inherit inputs;
+        };
+
         modules = [
+          agenix.nixosModules.default
           ./modules/common.nix
           ./modules/nfs.nix
           ./modules/podman.nix
           ./modules/ssh.nix
+          ./modules/tailscale.nix
           ./modules/users.nix
           ./modules/auto-upgrade.nix
           ./services/group_cobra.nix
