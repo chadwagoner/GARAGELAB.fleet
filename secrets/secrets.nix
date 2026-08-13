63a45daf1d0edf1db8fa03c0980186ca95eb02be
Chad Wagoner <chawagon03@gmail.com>
2026-08-13 01:07:04 -0400

    chore(secrets): add Tailscale OAuth secret

---
+++ secrets/secrets.nix
@@ -0,0 +1,10 @@
+let
+  admin = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFj6f+QSQTQzgUEXdI+ZaP4WEuyRL5p6X91NxlZOIG0Q";
+  cobra = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIIUS8G9GBmVNBtWg34UFVYn06WN9mLyKsIIeSBOCL2m3";
+
+in {
+  "tailscale-oauth.age".publicKeys = [
+    admin
+    cobra
+  ];
+}
