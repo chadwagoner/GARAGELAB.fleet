2f56dcfca9d52801f205f3c18e55c9f5470af208
Chad Wagoner <chawagon03@gmail.com>
2026-08-15 12:03:25 -0400

    fix(home-assistant): Rename Matter container

--- services/service_home-assistant-matter.nix
+++ services/service_home-assistant-matter.nix
@@ -1,7 +1,7 @@
 { lib, ... }:
 
 {
-  virtualisation.oci-containers.containers.ha-matter-server = {
+  virtualisation.oci-containers.containers.home-assistant-matter = {
     image = "ghcr.io/home-assistant-libs/python-matter-server:8.1@sha256:170aa093ce91c76cde4cc390918307590f0f5558fcec93f913af3cb019e6562a";
     extraOptions = [ "--security-opt=apparmor=unconfined" ];
     networks = [ "host" ];
@@ -11,6 +11,6 @@
     ];
   };
 
-  systemd.services.podman-ha-matter-server.serviceConfig.Restart =
+  systemd.services.podman-home-assistant-matter.serviceConfig.Restart =
     lib.mkForce "always";
 }
