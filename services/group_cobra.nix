e763901ed1d3c68f65becc2c61ffb6194043ff9a
Chad Wagoner <chawagon03@gmail.com>
2026-08-14 22:23:46 -0400

    feat(cobra): import media services

--- services/group_cobra.nix
+++ services/group_cobra.nix
@@ -4,8 +4,15 @@
   # Keep service imports explicit so adding a file does not deploy it by accident.
   imports = [
     ./service_adguard.nix
+    ./service_cleanuparr.nix
     ./service_docktail.nix
+    ./service_nzbget.nix
     ./service_plex.nix
     ./service_pocket-id.nix
+    ./service_profilarr.nix
+    ./service_prowlarr.nix
+    ./service_pulsarr.nix
+    ./service_radarr.nix
+    ./service_sonarr.nix
   ];
 }
