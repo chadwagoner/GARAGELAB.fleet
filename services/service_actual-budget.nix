24722640ccecde71d4e4beac64076499c63d0a85
Chad Wagoner <chawagon03@gmail.com>
2026-08-25 18:30:01 -0400

    fix(actual-budget): shorten docktail service name

--- services/service_actual-budget.nix
+++ services/service_actual-budget.nix
@@ -5,7 +5,7 @@
     image = "docker.io/actualbudget/actual-server:26.8.1@sha256:6478d9ddfc0924479c09e6699c205e354c6f2216dfe7de3c0fb7b590d6edcdc5";
     labels = {
       "docktail.service.enable" = "true";
-      "docktail.service.name" = "actual-budget";
+      "docktail.service.name" = "actual";
       "docktail.service.port" = "5006";
       "docktail.service.network" = "proxy";
       "docktail.service.protocol" = "http";
