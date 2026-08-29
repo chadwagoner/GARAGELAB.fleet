423850f6d76f6767b3d7e6682276ecdd41a0da57
Chad Wagoner <chawagon03@gmail.com>
2026-08-29 14:02:37 -0400

    fix(matter): Switch to matter-js server

--- services/service_home-assistant-matter.nix
+++ services/service_home-assistant-matter.nix
@@ -2,8 +2,7 @@
 
 {
   virtualisation.oci-containers.containers.home-assistant-matter = {
-    image = "ghcr.io/home-assistant-libs/python-matter-server:8.1@sha256:170aa093ce91c76cde4cc390918307590f0f5558fcec93f913af3cb019e6562a";
-    extraOptions = [ "--security-opt=apparmor=unconfined" ];
+    image = "ghcr.io/matter-js/matterjs-server:1.4.0";
     networks = [ "host" ];
     volumes = [
       "/run/dbus:/run/dbus:ro"
