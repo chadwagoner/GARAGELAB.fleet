1890ad1268ef585995bcb0c1aab4d7f2c0b4c653
Chad Wagoner <chawagon03@gmail.com>
2026-09-01 19:09:47 -0400

    feat(docktail): add ignored services option

--- services/service_docktail.nix
+++ services/service_docktail.nix
@@ -1,6 +1,15 @@
 { config, lib, ... }:
 
+let
+  cfg = config.fleet.service.docktail;
+in
 {
+  options.fleet.service.docktail.ignoredServiceNames = lib.mkOption {
+    type = lib.types.listOf lib.types.str;
+    default = [ ];
+    description = "Service names that Docktail should ignore when discovering services.";
+  };
+
   config = {
     age.secrets.docktail-oauth = {
       file = ../secrets/docktail-oauth.age;
@@ -13,6 +22,8 @@
       image = "ghcr.io/marvinvr/docktail:1.7.8@sha256:42b3ea342456abbd225060dee698123fb10602aa7d80f40313f965af775348f7";
       environment = {
         TAILSCALE_OAUTH_CLIENT_ID = "kYaEDocmEv11CNTRL";
+      } // lib.optionalAttrs (cfg.ignoredServiceNames != [ ]) {
+        IGNORE_SERVICE_NAMES = lib.concatStringsSep "," cfg.ignoredServiceNames;
       };
       environmentFiles = [
         config.age.secrets.docktail-oauth.path
